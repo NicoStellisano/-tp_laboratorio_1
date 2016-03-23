25be5f5dca276d4a2d1e53af3c9828b949bdb98a
NicoStellisano <nicostellisano@hotmail.com>
2016-03-23 17:20:07 -0300

    Revert "TP 1 Laboratorio"
    This reverts commit 9ed26397c09dd7db02306c1aa876bed2217115be.

--- funciones.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-#include "C:\Users\Nico\Desktop\TP_1_Cascara\libreriacrear.c"//Factorial
-#include "C:\Users\Nico\Desktop\TP_1_Cascara\suma.c"//Suma
-#include "C:\Users\Nico\Desktop\TP_1_Cascara\resta2.c"//Resta
-#include "C:\Users\Nico\Desktop\TP_1_Cascara\division.c"//Division
-#include "C:\Users\Nico\Desktop\TP_1_Cascara\multiplicacion.c"//Multiplicacion
-#endif
